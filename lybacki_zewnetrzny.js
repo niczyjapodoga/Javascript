4570e6b6af14199d0651831ab0bac0eadd675d71
Stanisław Łybacki <staslybacki@gmail.com>
2025-09-28 14:51:23 +0200

    Add files via upload

---
+++ lybacki_zewnetrzny.js
@@ -0,0 +1,3 @@
+// Stanisław Łybacki klasa 3F nr w dzienniku 16
+document.write("ostatnia modyfikacja strony".fontcolor("#00FFFF").strike().fontsize(7) + "<br>");
+document.write(document.lastModified);
